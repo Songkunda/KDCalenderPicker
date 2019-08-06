eda5b3bb21f1f89df1a9ad7eec29db0ac02be620
Kunda.Song <574779754@qq.com>
2019-08-06 12:06:46 +0800

    changed: jump 2 JTAppleCalendar

--- KDCalenderPicker/Classes/KDTimeRangeChooser.swift
+++ KDCalenderPicker/Classes/KDTimeRangeChooser.swift
@@ -6,7 +6,7 @@
 //
 
 import Foundation
-import Koyomi
+import JTAppleCalendar
 import UIKit
 
 class KDTimeRangeChooser: UIView {
@@ -17,8 +17,6 @@ class KDTimeRangeChooser: UIView {
     var beginDate:Date? = Date()
     var endDate:Date? = Date()
     
-    var startKoyomi: Koyomi?
-    var endKoyomi: Koyomi?
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupUI()
@@ -84,53 +82,19 @@ class KDTimeRangeChooser: UIView {
                 self.superview?.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:[cs]-(-88)-[pp(==512)]", options: [], metrics: nil, views: ["pp": popView, "cs": sCaLab]))
                 self.superview?.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[cs]-[pp(==280)]", options: [], metrics: nil, views: ["pp": popView, "cs": sCaLab]))
                 //
-                startKoyomi = {
-                    let (header, koyomi) = createKoyomi()
-                    popView.addSubview(header)
-                    popView.addSubview(koyomi)
-                    popView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-[hd(==40@1000)][sk(==200@1000)]", options: [], metrics: nil, views: ["hd": header, "sk": koyomi]))
-                    popView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[hd(==sk)]", options: [], metrics: nil, views: ["hd": header, "sk": koyomi]))
-                    return koyomi
-                }()
-                //
-                endKoyomi = {
-                    let (header, koyomi) = createKoyomi()
-                    popView.addSubview(header)
-                    popView.addSubview(koyomi)
-                    popView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-[hd(==40@1000)][ek(==200@1000)]", options: [], metrics: nil, views: ["hd": header, "ek": koyomi]))
-                    popView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:[hd(==ek)]|", options: [], metrics: nil, views: ["hd": header, "ek": koyomi]))
-                    return koyomi
-                }()
-                //
-                popView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[sk][ek(==sk@1000)]|", options: [], metrics: nil, views: ["sk": startKoyomi!, "ek": endKoyomi!]))
-                
-                let footer = UIView(frame: .zero)
-                footer.translatesAutoresizingMaskIntoConstraints = false
-                let cancelBtn = UIButton(type: .custom)
-                cancelBtn.translatesAutoresizingMaskIntoConstraints = false
-                cancelBtn.layer.cornerRadius = 2
-                cancelBtn.layer.borderColor = UIColor(red: 231.0 / 255.0, green: 231.0 / 255.0, blue: 231.0 / 255.0, alpha: 1.0).cgColor
-                cancelBtn.layer.borderWidth = 1
-                cancelBtn.setTitle("取消", for: .normal)
-                cancelBtn.setTitleColor(UIColor(red: 153.0 / 255.0, green: 153.0 / 255.0, blue: 153.0 / 255.0, alpha: 1), for: .normal)
-                cancelBtn.titleLabel?.font = UIFont.systemFont(ofSize: 12.0)
-                footer.addSubview(cancelBtn)
-                let submitBtn = UIButton(type: .custom)
-                submitBtn.translatesAutoresizingMaskIntoConstraints = false
-                submitBtn.setTitle("确定", for: .normal)
-                submitBtn.titleLabel?.font = UIFont.systemFont(ofSize: 12.0)
-                submitBtn.layer.cornerRadius = 2
-                submitBtn.backgroundColor = UIColor(red: 66.0 / 255.0, green: 192.0 / 255.0, blue: 155.0 / 255.0, alpha: 1.0)
-                footer.addSubview(submitBtn)
-                popView.addSubview(footer)
-                popView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[ft]|", options: [], metrics: nil, views: ["ft": footer]))
-                popView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[sk][ft]|", options: [], metrics: nil, views: ["ft": footer, "sk": startKoyomi!]))
-                footer.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:[cb(==56)]-20-[sb(==cb@1000)]-20-|", options: [], metrics: nil, views: ["cb": cancelBtn, "sb": submitBtn]))
-                footer.addConstraints([
-                    NSLayoutConstraint(item: submitBtn, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .height, multiplier: 0, constant: 25.0),
-                    NSLayoutConstraint(item: cancelBtn, attribute: .centerY, relatedBy: .equal, toItem: footer, attribute: .centerY, multiplier: 1, constant: 0),
-                    NSLayoutConstraint(item: cancelBtn, attribute: .height, relatedBy: .equal, toItem: submitBtn, attribute: .height, multiplier: 1, constant: 0),
-                    NSLayoutConstraint(item: submitBtn, attribute: .centerY, relatedBy: .equal, toItem: footer, attribute: .centerY, multiplier: 1, constant: 0)])
+                let beginCalendar = JTAppleCalendarView(frame: .zero)
+                beginCalendar.translatesAutoresizingMaskIntoConstraints = false
+                beginCalendar.register(DateCell.self, forCellWithReuseIdentifier: "dateCell")
+                beginCalendar.ibCalendarDelegate = self
+                beginCalendar.ibCalendarDataSource = self
+            
+                popView.addSubview(beginCalendar)
+                popView.addConstraints([
+                    NSLayoutConstraint(item: beginCalendar, attribute: .leading, relatedBy: .equal, toItem: popView, attribute: .leading, multiplier: 1, constant: 0),
+                     NSLayoutConstraint(item: beginCalendar, attribute: .top, relatedBy: .equal, toItem: popView, attribute: .top, multiplier: 1, constant: 0),
+                      NSLayoutConstraint(item: beginCalendar, attribute: .width, relatedBy: .equal, toItem: popView, attribute: .width, multiplier: 0.5, constant: 0),
+                    NSLayoutConstraint(item: beginCalendar, attribute: .bottom, relatedBy: .equal, toItem: popView, attribute: .bottom, multiplier: 1, constant: 0),
+                    ])
                 
                 return popView
             }()
@@ -143,40 +107,7 @@ class KDTimeRangeChooser: UIView {
         }
     }
     
-    func createKoyomi() -> (KoyomiHeader, Koyomi) {
-        let koyomiHeader = KoyomiHeader()
-        koyomiHeader.translatesAutoresizingMaskIntoConstraints = false
-        
-        let koyomi = Koyomi(frame: .zero, sectionSpace: 0, cellSpace: 0, inset: .init(top: 8, left: 8, bottom: 8, right: 8), weekCellHeight: 30)
-        koyomi.setDayFont(fontName: "Symbol", size: 12.0)
-        koyomi.setWeekFont(fontName: "Symbol", size: 12.0)
-        koyomi.translatesAutoresizingMaskIntoConstraints = false
-        // 文字
-        koyomi.weeks = ("日", "一", "二", "三", "四", "五", "六")
-        koyomi.currentDateFormat = "yyyy年M月"
-        // 选择今日
-        let today = Date()
-        koyomi.select(date: today)
-        // 颜色
-        koyomi.selectedStyleColor = UIColor(red: 66.0 / 255.0, green: 192.0 / 255.0, blue: 155.0 / 255.0, alpha: 1.0)
-        let textBlack = UIColor(red: 51.0 / 255.0, green: 51.0 / 255.0, blue: 51.0 / 255.0, alpha: 1.0)
-        let textGray = UIColor(red: 195.0 / 255.0, green: 206.0 / 255.0, blue: 203.0 / 255.0, alpha: 1.0)
-        let customColorScheme: KoyomiStyle.CustomColorScheme = (dayBackgrond: UIColor.white,
-                                                                weekBackgrond: UIColor.white,
-                                                                week: textGray,
-                                                                weekday: textBlack,
-                                                                holiday: (saturday: textBlack, sunday: textBlack),
-                                                                otherMonth: textGray,
-                                                                separator: .white)
-        
-        koyomi.style = KoyomiStyle.custom(customColor: customColorScheme)
-        //
-        koyomi.layer.borderColor = UIColor(red: 231.0 / 255.0, green: 231.0 / 255.0, blue: 231.0 / 255.0, alpha: 1.0).cgColor
-        koyomi.layer.borderWidth = 1
-        koyomi.alpha = 1
-        //
-        return (koyomiHeader, koyomi)
-    }
+
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -184,54 +115,58 @@ class KDTimeRangeChooser: UIView {
     }
 }
 
-class KoyomiHeader: UIView {
-    let lockValue = 9000
-    var isSetup = false
-    weak var koymi: Koyomi?
+extension KDTimeRangeChooser: JTAppleCalendarViewDataSource {
+    func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy MM dd"
+        
+        let startDate = formatter.date(from: "2018 01 01")!
+        let endDate = Date()
+        return ConfigurationParameters(startDate: startDate, endDate: endDate)
+    }
     
+}
+
+extension KDTimeRangeChooser: JTAppleCalendarViewDelegate {
+    func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTAppleCell {
+        let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "dateCell", for: indexPath) as! DateCell
+        self.calendar(calendar, willDisplay: cell, forItemAt: date, cellState: cellState, indexPath: indexPath)
+        return cell
+    }
+    func calendar(_ calendar: JTAppleCalendarView, willDisplay cell: JTAppleCell, forItemAt date: Date, cellState: CellState, indexPath: IndexPath) {
+        let cell = cell as! DateCell
+        cell.dateLabel.text = cellState.text
+        configureCell(view: cell, cellState: cellState)
+    }
+}
+extension KDTimeRangeChooser { //for JTAppleCalendarViewDelegate
+    func configureCell(view: JTAppleCell?, cellState: CellState) {
+        guard let cell = view as? DateCell  else { return }
+        cell.dateLabel.text = cellState.text
+        handleCellTextColor(cell: cell, cellState: cellState)
+    }
+    
+    func handleCellTextColor(cell: DateCell, cellState: CellState) {
+        if cellState.dateBelongsTo == .thisMonth {
+            cell.dateLabel.textColor = UIColor.black
+        } else {
+            cell.dateLabel.textColor = UIColor.gray
+        }
+    }
+}
+
+class DateCell: JTAppleCell {
+    var dateLabel: UILabel = UILabel(frame: .zero)
     override init(frame: CGRect) {
         super.init(frame: frame)
-        setupUI()
+        dateLabel.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(dateLabel)
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[dl]|", options: [], metrics: nil, views: ["dl":dateLabel]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[dl]|", options: [], metrics: nil, views: ["dl":dateLabel]))
     }
     
     required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        setupUI()
+        fatalError("init(coder:) has not been implemented")
     }
     
-    func setupUI() {
-        objc_sync_enter(lockValue)
-        if isSetup == false {
-            isSetup = true
-            //
-            layer.borderColor = UIColor(red: 231.0 / 255.0, green: 231.0 / 255.0, blue: 231.0 / 255.0, alpha: 1.0).cgColor
-            layer.borderWidth = 1
-            alpha = 1
-            //
-            let leftBtn = UIButton(type: .custom)
-            leftBtn.translatesAutoresizingMaskIntoConstraints = false
-            leftBtn.setTitle("<", for: .normal)
-            leftBtn.titleLabel?.font = UIFont.systemFont(ofSize: 12.0)
-            leftBtn.setTitleColor(UIColor(red: 51.0 / 255.0, green: 51.0 / 255.0, blue: 51.0 / 255.0, alpha: 1.0), for: .normal)
-            addSubview(leftBtn)
-            let titleLab = UILabel(frame: .zero)
-            titleLab.translatesAutoresizingMaskIntoConstraints = false
-            titleLab.text = "2019年7月"
-            titleLab.textAlignment = .center
-            titleLab.font = .systemFont(ofSize: 12)
-            titleLab.sizeToFit()
-            addSubview(titleLab)
-            let rightBtn = UIButton(type: .custom)
-            rightBtn.translatesAutoresizingMaskIntoConstraints = false
-            rightBtn.setTitle(">", for: .normal)
-            rightBtn.titleLabel?.font = UIFont.systemFont(ofSize: 12.0)
-            rightBtn.setTitleColor(UIColor(red: 51.0 / 255.0, green: 51.0 / 255.0, blue: 51.0 / 255.0, alpha: 1.0), for: .normal)
-            addSubview(rightBtn)
-            addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-15-[lb(==45)]-15-[ti]-15-[rb(==45)]-15-|", options: [], metrics: nil, views: ["lb": leftBtn, "rb": rightBtn, "ti": titleLab]))
-            addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[subV]|", options: [], metrics: nil, views: ["subV": leftBtn]))
-            addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[subV]|", options: [], metrics: nil, views: ["subV": titleLab]))
-            addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[subV]|", options: [], metrics: nil, views: ["subV": rightBtn]))
-        }
-        objc_sync_exit(lockValue)
-    }
 }
