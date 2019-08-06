c9e6a8962f2b08aa4fc7833ec0a18590e5f0763a
Kunda.Song <574779754@qq.com>
2019-08-06 09:35:26 +0800

    update

--- KDCalenderPicker/Classes/KDTimeRangeChooser.swift
+++ KDCalenderPicker/Classes/KDTimeRangeChooser.swift
@@ -1,11 +1,12 @@
 //
-import Foundation
-import Koyomi
 //  KDTimeRangeChooser.swift
 //  FBSnapshotTestCase
 //
 //  Created by Kunda.Song on 2019/8/2.
 //
+
+import Foundation
+import Koyomi
 import UIKit
 
 class KDTimeRangeChooser: UIView {
@@ -13,14 +14,14 @@ class KDTimeRangeChooser: UIView {
     var isSetup = false
     var poper: UIView?
     let sCaLab = UILabel(frame: .zero)
-
+    
     var startKoyomi: Koyomi?
     var endKoyomi: Koyomi?
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupUI()
     }
-
+    
     func setupUI() {
         if isSetup == false {
             isSetup = true
@@ -65,7 +66,7 @@ class KDTimeRangeChooser: UIView {
             NSLayoutConstraint.activate(NSLayoutConstraint.constraints(withVisualFormat: "V:[sf(>=24@1000)]", options: [.alignAllCenterY], metrics: nil, views: ["sf": self]))
         }
     }
-
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
         if poper == nil {
@@ -82,62 +83,125 @@ class KDTimeRangeChooser: UIView {
                 self.superview?.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[cs]-[pp(==280)]", options: [], metrics: nil, views: ["pp": popView, "cs": sCaLab]))
                 //
                 startKoyomi = {
-                    let koyomi = Koyomi(frame: .zero, sectionSpace: 0, cellSpace: 0, inset: .zero, weekCellHeight: 20)
-                    koyomi.translatesAutoresizingMaskIntoConstraints = false
-                    koyomi.weeks = ("日", "一", "二", "三", "四", "五", "六")
-                    koyomi.currentDateFormat = "yyyy年M月"
-                    //选择今日
-                    let today = Date()
-                    koyomi.select(date: today)
-                    //
-                    koyomi.selectedStyleColor = UIColor(red: 66.0 / 255.0, green: 192.0 / 255.0, blue: 155.0 / 255.0, alpha: 1.0)
-                    let textBlack = UIColor(red: 51.0 / 255.0, green: 51.0 / 255.0, blue: 51.0 / 255.0, alpha: 1.0)
-                    let textGray = UIColor(red: 195.0 / 255.0, green: 206.0 / 255.0, blue: 203.0 / 255.0, alpha: 1.0)
-                    let customColorScheme:KoyomiStyle.CustomColorScheme = (dayBackgrond: UIColor.white,
-                                                                           weekBackgrond: UIColor.white,
-                                                                           week: textGray,
-                                                                           weekday: textBlack,
-                                                                           holiday: (saturday: textBlack, sunday: textBlack),
-                                                                           otherMonth:textGray,
-                                                                           separator: UIColor.green)
-                    
-                    koyomi.style = KoyomiStyle.custom(customColor: customColorScheme)
-                    //
+                    let (header, koyomi) = createKoyomi()
+                    popView.addSubview(header)
                     popView.addSubview(koyomi)
-                    popView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[sk(==240@1000)]", options: [], metrics: nil, views: ["sk":koyomi]))
+                    popView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-[hd(==40@1000)][sk(==200@1000)]", options: [], metrics: nil, views: ["hd": header, "sk": koyomi]))
+                    popView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[hd(==sk)]", options: [], metrics: nil, views: ["hd": header, "sk": koyomi]))
                     return koyomi
                 }()
                 //
                 endKoyomi = {
-                    let koyomi = Koyomi(frame: .zero, sectionSpace: 1.5, cellSpace: 0.5, inset: .zero, weekCellHeight: 25)
-                    koyomi.translatesAutoresizingMaskIntoConstraints = false
+                    let (header, koyomi) = createKoyomi()
+                    popView.addSubview(header)
                     popView.addSubview(koyomi)
-                    popView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[ek(==240@1000)]", options: [], metrics: nil, views: ["ek":koyomi]))
+                    popView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-[hd(==40@1000)][ek(==200@1000)]", options: [], metrics: nil, views: ["hd": header, "ek": koyomi]))
+                    popView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:[hd(==ek)]|", options: [], metrics: nil, views: ["hd": header, "ek": koyomi]))
                     return koyomi
                 }()
                 //
-                popView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[sk][ek(==sk@1000)]|", options: [], metrics: nil, views: ["sk":startKoyomi!,"ek":endKoyomi!]))
-                
+                popView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[sk][ek(==sk@1000)]|", options: [], metrics: nil, views: ["sk": startKoyomi!, "ek": endKoyomi!]))
                 
                 return popView
             }()
-           
-
-        } else {
             
+        } else {
             poper = {
-                
-                
-                
-                
                 poper?.removeFromSuperview()
                 return nil
             }()
         }
     }
-
+    
+    func createKoyomi() -> (KoyomiHeader, Koyomi) {
+        let koyomiHeader = KoyomiHeader()
+        koyomiHeader.translatesAutoresizingMaskIntoConstraints = false
+        
+        let koyomi = Koyomi(frame: .zero, sectionSpace: 0, cellSpace: 0, inset: .init(top: 8, left: 8, bottom: 8, right: 8), weekCellHeight: 30)
+        koyomi.setDayFont(fontName: "Symbol", size: 12.0)
+        koyomi.setWeekFont(fontName: "Symbol", size: 12.0)
+        koyomi.translatesAutoresizingMaskIntoConstraints = false
+        // 文字
+        koyomi.weeks = ("日", "一", "二", "三", "四", "五", "六")
+        koyomi.currentDateFormat = "yyyy年M月"
+        // 选择今日
+        let today = Date()
+        koyomi.select(date: today)
+        // 颜色
+        koyomi.selectedStyleColor = UIColor(red: 66.0 / 255.0, green: 192.0 / 255.0, blue: 155.0 / 255.0, alpha: 1.0)
+        let textBlack = UIColor(red: 51.0 / 255.0, green: 51.0 / 255.0, blue: 51.0 / 255.0, alpha: 1.0)
+        let textGray = UIColor(red: 195.0 / 255.0, green: 206.0 / 255.0, blue: 203.0 / 255.0, alpha: 1.0)
+        let customColorScheme: KoyomiStyle.CustomColorScheme = (dayBackgrond: UIColor.white,
+                                                                weekBackgrond: UIColor.white,
+                                                                week: textGray,
+                                                                weekday: textBlack,
+                                                                holiday: (saturday: textBlack, sunday: textBlack),
+                                                                otherMonth: textGray,
+                                                                separator:.white)
+        
+        koyomi.style = KoyomiStyle.custom(customColor: customColorScheme)
+        //
+        koyomi.layer.borderColor = UIColor(red: 231.0 / 255.0, green: 231.0 / 255.0, blue: 231.0 / 255.0, alpha: 1.0).cgColor
+        koyomi.layer.borderWidth = 1
+        koyomi.alpha = 1
+        //
+        return (koyomiHeader, koyomi)
+    }
+    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         setupUI()
+    }
+}
+
+class KoyomiHeader: UIView {
+    let lockValue = 9000
+    var isSetup = false
+    weak var koymi: Koyomi?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupUI()
+    }
+    
+    func setupUI() {
+        objc_sync_enter(lockValue)
+        if isSetup == false {
+            isSetup = true
+            //
+            layer.borderColor = UIColor(red: 231.0 / 255.0, green: 231.0 / 255.0, blue: 231.0 / 255.0, alpha: 1.0).cgColor
+            layer.borderWidth = 1
+            alpha = 1
+            //
+            let leftBtn = UIButton(type: .custom)
+            leftBtn.translatesAutoresizingMaskIntoConstraints = false
+            leftBtn.setTitle("<", for: .normal)
+            leftBtn.titleLabel?.font = UIFont.systemFont(ofSize: 12.0)
+            leftBtn.setTitleColor(UIColor(red: 51.0 / 255.0, green: 51.0 / 255.0, blue: 51.0 / 255.0, alpha: 1.0), for: .normal)
+            addSubview(leftBtn)
+            let titleLab = UILabel(frame: .zero)
+            titleLab.translatesAutoresizingMaskIntoConstraints = false
+            titleLab.text = "2019年7月"
+            titleLab.textAlignment = .center
+            titleLab.font = .systemFont(ofSize: 12)
+            titleLab.sizeToFit()
+            addSubview(titleLab)
+            let rightBtn = UIButton(type: .custom)
+            rightBtn.translatesAutoresizingMaskIntoConstraints = false
+            rightBtn.setTitle(">", for: .normal)
+            rightBtn.titleLabel?.font = UIFont.systemFont(ofSize: 12.0)
+            rightBtn.setTitleColor(UIColor(red: 51.0 / 255.0, green: 51.0 / 255.0, blue: 51.0 / 255.0, alpha: 1.0), for: .normal)
+            addSubview(rightBtn)
+            addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-15-[lb(==45)]-15-[ti]-15-[rb(==45)]-15-|", options: [], metrics: nil, views: ["lb": leftBtn, "rb": rightBtn, "ti": titleLab]))
+            addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[subV]|", options: [], metrics: nil, views: ["subV": leftBtn]))
+            addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[subV]|", options: [], metrics: nil, views: ["subV": titleLab]))
+            addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[subV]|", options: [], metrics: nil, views: ["subV": rightBtn]))
+        }
+        objc_sync_exit(lockValue)
     }
 }
